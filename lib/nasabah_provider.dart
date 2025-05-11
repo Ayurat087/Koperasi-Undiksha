d1a2099004d249e8c86e2a519a9eac59a53598b4
Ayurat087 <ayu.rat.resmi@student.undiksha.ac.id>
2025-05-11 19:25:26 +0800

    Update nasabah_provider.dart

--- lib/nasabah_provider.dart
+++ lib/nasabah_provider.dart
@@ -4,14 +4,9 @@ class NasabahProvider extends ChangeNotifier {
   String nama = 'AYU RAT RESMI ARDHA SWARI';
   double saldo = 20000000;
 
-  // Update the saldo attribute by adding or subtracting a value
-  void updateSaldo(double amount) {
-    saldo += amount; // Adjust this line based on your logic (addition or subtraction)
-    notifyListeners(); {
-      
-    }
-    
-    
+  void updateSaldo(double baru) {
+    saldo = baru;
+    notifyListeners();
   }
 }
 
